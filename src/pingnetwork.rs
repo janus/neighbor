4556b12e76063db9bbaa77a24d6a9c61979c6268
janus <emekamicro@gmail.com>
2017-10-13 22:10:23 +0100

    worked on merging branch with remote Revised code, removed unnecessary code lines

---
+++ src/pingnetwork.rs
@@ -0,0 +1,198 @@
+use bytes::{BytesMut, Buf, BufMut};
+use mio::{Events, Poll, PollOpt, Ready, Token};
+use std::net::SocketAddr;
+use mio::udp::*;
+use serialization;
+use time;
+use neighbors::Neighbors;
+use neighbor::decode_str;
+
+
+const BUFFER_CAPACITY: usize = 800;
+const LISTENER: Token = Token(0);
+const SENDER: Token = Token(1);
+
+
+pub fn UDPsocket(ipadr: &str, port: &str) -> (UdpSocket, SocketAddr) {
+    let ip_and_port = format!("{}:{}", ipadr, port);
+    let saddr: SocketAddr = ip_and_port.parse().unwrap();
+    let socket = match UdpSocket::bind(&saddr) {
+        Ok(s) => s,
+        Err(e) => panic!("Failed to establish bind socket {}", e),
+    };
+    (socket, saddr)
+}
+
+pub fn dcod(mstr: &str) -> String {
+    let dstr = match decode_str(&mstr){
+        Some(v) => v,
+        _ => { panic!("Failed to decode {}", mstr); }
+    };
+    dstr
+}
+
+pub struct Multicast_Net {
+    tx: UdpSocket,
+    rx: UdpSocket,
+    secret: [u8; 64],
+    shutdown: bool,
+    packet_sent: bool,
+    buf: BytesMut,
+    st_time: i64,
+    ipaddr: SocketAddr,
+    pub nodes: Neighbors,
+}
+
+impl Multicast_Net {
+    pub fn new(
+        rx_ip: &str,
+        rx_udp: &str,
+        pro_vec: Vec<&str>,
+        secret: [u8; 64],
+    ) -> Multicast_Net {
+        let (rx_udpsock, _) = UDPsocket(rx_ip, rx_udp);
+        let (tx_udpsock, ip_addr) = UDPsocket(&dcod(&pro_vec[2]), &dcod(&pro_vec[3]));
+
+        Multicast_Net {
+            tx: tx_udpsock,
+            rx: rx_udpsock,
+            secret: secret,
+            ipaddr: ip_addr,
+            buf: serialization::payload(&pro_vec, 0, &secret, "ipv4_hello"),
+            shutdown: false,
+            packet_sent: false,
+            st_time: time::get_time().sec,
+            nodes: Neighbors::new(),
+        }
+    }
+
+    pub fn parse_packet(&mut self, buf: BytesMut) {
+        match serialization::on_pong(buf, self.nodes.get_host_status_num()) {
+            Some(ngb) => {
+                self.nodes.insert_neighbor(ngb);
+            }
+            _ => {}
+        };
+    }
+
+    pub fn read_udpsocket(&mut self, _: &mut Poll, token: Token, _: Ready) {
+        let mut current_time = 0 as i64;
+        match token {
+            LISTENER => {
+                let mut buf: BytesMut = BytesMut::with_capacity(BUFFER_CAPACITY);
+                match self.rx.recv_from(&mut buf[..]) {
+                    Ok(Some((len, address))) => {
+                        current_time = time::get_time().sec;
+                        self.parse_packet(buf);
+                    }
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error reading UDP packet: {:?}", e);
+                    }
+                };
+                if current_time > self.st_time {
+                    self.shutdown = true;
+                }
+            }
+            _ => (),
+        }
+    }
+
+    pub fn send_packet(&mut self, _: &mut Poll, token: Token, _: Ready) {
+        if self.packet_sent {
+            return;
+        }
+        match token {
+            SENDER => {
+                match self.tx.send_to(&self.buf[..], &self.ipaddr) {
+                    Ok(Some(size)) if size == self.buf.len() => {
+                        self.packet_sent = true;
+                        self.st_time = time::get_time().sec + 120;
+                    }
+                    Ok(Some(_)) => {
+                        println!("UDP sent incomplete payload");
+                    }
+                    Ok(None) => {
+                        println!("UDP sent Nothing");;
+                    }
+                    Err(e) => {
+                        println!(
+                            "Error send UDP:: {:?} and the sock_addr is {:?}",
+                            e,
+                            &self.ipaddr
+                        );
+                    }
+                };
+            }
+            _ => (),
+        }
+    }
+
+    pub fn start_net(&mut self) {
+        let mut poll = Poll::new().unwrap();
+
+        poll.register(&self.tx, SENDER, Ready::writable(), PollOpt::edge())
+            .unwrap();
+
+        poll.register(&self.rx, LISTENER, Ready::readable(), PollOpt::edge())
+            .unwrap();
+
+        let mut events = Events::with_capacity(1024);
+
+        while !self.shutdown {
+            poll.poll(&mut events, None).unwrap();
+            for event in &events {
+                if event.readiness().is_readable() {
+                    self.read_udpsocket(&mut poll, event.token(), event.readiness());
+                }
+                if event.readiness().is_writable() {
+                    self.send_packet(&mut poll, event.token(), event.readiness());
+                }
+            }
+        }
+    }
+
+    pub fn close(self) {
+        drop(self.tx);
+        drop(self.rx);
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+	use time;
+	use serialization;
+	use edcert::ed25519;
+	use base64::{decode, encode};
+	use bytes::{BufMut, BytesMut};
+	use pingnetwork::{Multicast_Net};
+
+
+	fn encodeVal(
+		udp_port: &str, 
+		ip_address: &str
+	)-> (String, String, String, [u8; 64]){
+		let (psk, msk) = ed25519::generate_keypair();
+		return (encode(&ip_address), encode(&udp_port), encode(&psk), msk);
+	
+	}
+	
+    #[test]
+	fn test_network_returned() {
+		let (ip_addr, udp_port, pub_key, secret) = encodeVal("41235", "224.0.0.3");
+		let cloned_pub_key = pub_key.clone();
+		let mut vec: Vec<&str> = Vec::new();
+		vec.push(&pub_key);
+		vec.push(&cloned_pub_key);
+		vec.push(&ip_addr);
+		vec.push(&udp_port);
+		let bytes = serialization::payload(&vec, 45, &secret, "hello_confirm");
+		let mut network = Multicast_Net::new("224.0.0.8","41239", vec, secret );
+        network.parse_packet(bytes);
+        assert_eq!(1, network.nodes.get_neighbors().len());
+	}
+
+
+	
+}
---
+++ src/serialization.rs
@@ -0,0 +1,189 @@
+use bytes::{BufMut, BytesMut};
+use neighbor::Neighbor;
+use time;
+use std::str;
+use edcert::ed25519;
+use base64::{decode, encode};
+
+const BUFFER_CAPACITY_MESSAGE: usize = 400;
+
+pub fn decode_key(mstr: &str) -> Option<Vec<u8>> {
+    match decode(mstr) {
+        Ok(v) => {
+            return Some(v);
+        }
+        Err(e) => {
+            println!("Failed to decode  {}", e);
+            return None;
+        }
+    };
+}
+
+pub fn payload(
+    profile: &Vec<&str>,
+    seqnum: i32,
+    secret: &[u8; 64],
+    header_msg: &str,
+) -> BytesMut {
+    let sig;
+    let tme = time::get_time().sec + 70;
+    let mut rslt = BytesMut::with_capacity(BUFFER_CAPACITY_MESSAGE);
+    let msg = format!(
+        "{} {} {} {} {} {} {}",
+        header_msg,
+        profile[0],
+        profile[1],
+        profile[2],
+        profile[3],
+        tme,
+        seqnum
+    );
+    sig = ed25519::sign(msg.as_bytes(), secret);
+    rslt.put(msg);
+    rslt.put(" ");
+    rslt.put(encode(&sig));
+    rslt
+}
+
+pub fn on_pong(packet: BytesMut, active: i32) -> Option<Neighbor> {
+    let vec_str: Vec<&str>;
+    let payload;
+    let pub_key;
+    let sig;
+    let tm;
+    if check_size(&packet) && match_header(&packet) {
+        vec_str = bytes_vec(&packet);
+        payload = extract_payload(&vec_str);
+        match vec_str[vec_str.len() - 3].parse::<i64>() {
+            Ok(v) => {
+                tm = time::get_time().sec;
+                if !time_within(v, tm) {
+                    return None;
+                }
+            },
+            Err(e) => {
+                println!("Bad protocol: failed to extract {:?}", e);
+                return None;
+            }
+        };
+        pub_key = match decode_key(&vec_str[1]) {
+            Some(v) => v,
+            _ => { return None; }
+        };
+        sig = match decode_key(&vec_str[vec_str.len() - 1]) {
+            Some(v) => v,
+            _ => { return None; }
+        };
+        if ed25519::verify(payload.as_bytes(), &sig, &pub_key) {
+
+            match Neighbor::new(vec_str, active) {
+                Some(ngb) => { return Some(ngb);}
+                _ => { return None; }
+            };
+        }
+    }
+    return None;
+}
+
+pub fn match_header(packet: &BytesMut) -> bool {
+    match str::from_utf8(&packet[0..13]) {
+        Ok(v) => {
+            return  "hello_confirm" == v;
+        },
+        Err(e) => {
+            println!("Found invalid UTF-8 {:?}", e);
+            return false;
+        }
+    };
+}
+
+pub fn check_size(packet: &BytesMut) -> bool {
+    packet.len() > 200
+}
+
+pub fn time_within(sent_tm: i64, now_tm: i64) -> bool {
+    now_tm <= sent_tm
+}
+
+fn bytes_vec(packet: &BytesMut) -> Vec<&str> {
+    let str_buf = match str::from_utf8(&packet[..]) {
+        Ok(v) => v,
+        Err(e) => {
+            println!("Found invalid UTF-8 {:?}", e);
+            ""
+        }
+    };
+    str_buf.split_whitespace().collect()
+}
+
+fn extract_payload(vec: &Vec<&str>) -> String {
+    vec[0..7].join(" ")
+}
+
+#[cfg(test)]
+mod test {
+
+    use time;
+    use serialization;
+    use edcert::ed25519;
+    use base64::{decode, encode};
+    use bytes::{BufMut, BytesMut};
+
+    fn encodeVal(udp_port: &str, ip_address: &str) -> (String, String, String, [u8; 64]) {
+        let (psk, msk) = ed25519::generate_keypair();
+        return (encode(&ip_address), encode(&udp_port), encode(&psk), msk);
+
+    }
+
+    fn pong_host() -> (BytesMut, String, [u8; 64]) {
+        let (ip_addr, udp_port, pub_key, secret) =
+            encodeVal("41235", "224.0.0.3");
+        let mut vec: Vec<&str> = Vec::new();
+        let pb_key = pub_key.clone();
+        vec.push(&pub_key);
+        vec.push(&pub_key); // Used as payment address
+        vec.push(&ip_addr);
+        vec.push(&udp_port);
+        let bytes = serialization::payload(&vec, 45, &secret, "hello_confirm");
+        return (bytes, pb_key, secret);
+    }
+
+    fn not_time() -> Option<i64> {
+        let n = 40;
+        if n > 20 {
+            return Some(n);
+        } else {
+            return None;
+        }
+    }
+
+    fn time_within(sent_tm: i64, now_tm: i64) -> bool {
+        if now_tm <= sent_tm { true } else { false }
+    }
+
+    #[test]
+    fn serialization_test_header_msg() {
+        let (mbytes, _, _) = pong_host();
+        let header_str = String::from_utf8(mbytes[0..13].to_vec()).expect("Found invalid UTF-8");
+        assert_eq!(header_str, "hello_confirm");
+    }
+
+    #[test]
+    fn serialization_test_time() {
+        let (mbytes, _, _) = pong_host();
+        let vec = serialization::bytes_vec(&mbytes);
+        let packet_tm = vec[vec.len() - 3].parse::<i64>().unwrap();
+        let now_tm = time::get_time().sec as i64;
+        assert_eq!(time_within(packet_tm, now_tm), true);
+    }
+
+    #[test]
+    fn serialization_test_packet_header() {
+        let (mbytes, _, _) = pong_host();
+
+        match serialization::on_pong(mbytes, 8) {
+            Some(ngb) => assert_eq!(ngb.get_endpoint().ip_address, "224.0.0.3"),
+            _ => assert!(false),
+        };
+    }
+}
